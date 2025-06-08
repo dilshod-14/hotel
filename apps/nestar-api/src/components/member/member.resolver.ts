4dcf9bcc2a68291a93a366eefa6f5d0c796824e1
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-09 02:35:26 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,30 +1,34 @@
-import { Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
 	@Mutation(() => String)
-	public async signup(): Promise<string> {
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<string> {
 		console.log('Mutation: signup');
+        console.log('input:', input);
 		return this.memberService.signup();
+	}
 
-    }
-
-    @Mutation(() => String)
-	public async login(): Promise<string> {
+	@Mutation(() => String)
+    @UsePipes(ValidationPipe)
+	public async login(@Args('input') input: LoginInput): Promise<string> {
 		console.log('Mutation: login');
 		return this.memberService.login();
 	}
 
-      @Mutation(() => String)
+	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 
-     @Query(() => String)
+	@Query(() => String)
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
