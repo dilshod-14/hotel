630aa1cd7cb3c5ddd9e46ee5989a3dac25989275
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-10 00:23:20 +0900

    feat: TASCK

--- train.ts
+++ train.ts
@@ -1,9 +1,19 @@
-function printNumbers() {
-  let i = 1;
-  const timer = setInterval(() => {
-    console.log(i++);
-    if (i > 5) clearInterval(timer);
-  }, 1000);
+// function printNumbers() {
+//   let i = 1;
+//   const timer = setInterval(() => {
+//     console.log(i++);
+//     if (i > 5) clearInterval(timer);
+//   }, 1000);
+// }
+
+// printNumbers();
+
+
+function reverseInteger(num: number): number {
+    const reversed = Number(String(Math.abs(num)).split('').reverse().join(''));
+    return num < 0 ? -reversed : reversed;
 }
 
-printNumbers();
+
+console.log(reverseInteger(123456789)); 
+console.log(reverseInteger(-98765));    
