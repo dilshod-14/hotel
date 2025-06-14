7eea75d145948c5b4d4d4664502045b61ee0de1d
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-14 15:10:21 +0900

    feat: create getAllMembersByAdmin and updateMembersByAdmin GrapfQl ApIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -18,6 +18,7 @@ export class MemberResolver {
 		return this.memberService.login(input);
 	}
 
+	//Authenticatad  (USER, AGENT, ADMIN)
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
@@ -28,5 +29,19 @@ export class MemberResolver {
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
+	}
+
+	/**ADMIN */
+
+	//AUTHORIZATION: Admin
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(): Promise<string> {
+		return this.memberService.getAllMembersByAdmin();
+	}
+
+	@Mutation(() => String)
+	public async updateMembersByAdmin(): Promise<string> {
+		console.log('Mutation: updateMembersByAdmin');
+		return this.memberService.updateMembersByAdmin();
 	}
 }
