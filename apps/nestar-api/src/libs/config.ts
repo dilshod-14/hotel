3657d9a7b2ce55df4a3cae023705b8b7a5360853
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-17 00:44:34 +0900

    feat: develop getAgents GrephQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from 'bson';
 
+export const availableAgentSort = ['createdAt', 'updatedAt', 'memberLikes', 'memberView', 'memberRank'];
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -1,7 +1,9 @@
 import { MemberAuthType } from './../../enums/member.enum';
-import { Field, InputType } from '@nestjs/graphql';
-import { IsNotEmpty, IsOptional, Length } from 'class-validator';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsIn, isNotEmpty, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
 import { MemberType } from '../../enums/member.enum';
+import { availableAgentSort } from '../../config';
+import { Direction } from '../../enums/common.enum';
 
 @InputType()
 export class MemberInput {
@@ -39,4 +41,37 @@ export class LoginInput {
 	@Length(5, 12)
 	@Field(() => String)
 	memberPassword: string;
+}
+
+@InputType()
+class AISearch {
+	@IsNotEmpty()
+	@Field(() => String, { nullable: true })
+	text?: string;
+}
+
+@InputType()
+export class AgentsInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
+
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
+
+	@IsOptional()
+	@IsIn(availableAgentSort)
+	@Field(() => String, { nullable: true })
+	sort?: string;
+
+	@IsOptional()
+	@Field(() => Direction, { nullable: true })
+	direction?: Direction;
+
+	@IsNotEmpty()
+	@Field(() => AISearch)
+	search: AISearch;
 }
