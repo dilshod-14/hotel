c9e25f83dfbf5ff10bdb4a4fd3aeb7d4211dcd64
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-20 23:54:19 +0900

    feat: develop single and multi image uplader mutation GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,15 @@
 import { ObjectId } from 'bson';
 
+/** // IMAGE CONFIGURATION  */
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 export const shapeIntoMongoObjectId = (target: any) => {
