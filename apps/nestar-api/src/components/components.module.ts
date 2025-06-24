fe5f30141187ce9660fe12e83723c864f5962da7
dilshod-14 <urolovdilshod24@gmail.com>
2025-06-25 07:14:35 +0900

    feat: configure boardArticle module and integrate boartArticle DTO

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -6,14 +6,14 @@ import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/like.module';
 import { ViewModule } from './view/view.module';
 import { FollowModule } from './follow/follow.module';
-import { BoartArticleModule } from './boart-article/boart-article.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
 	imports: [
 		MemberModule,
 		AuthModule,
 		PropertyModule,
-		BoartArticleModule,
+		BoardArticleModule, 
 		CommentModule,
 		LikeModule,
 		ViewModule,
--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -5,8 +5,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
-import { MemberResolver } from '../member/member.resolver';
-import { MemberService } from '../member/member.service';
 import PropertySchema from '../../schemas/Property.model';
 import { MemberModule } from '../member/member.module';
 
