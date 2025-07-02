8140ef1bf750d9aab24b4205c90e2299ae19dafe
dilshod-14 <urolovdilshod24@gmail.com>
2025-07-02 20:26:25 +0900

    feat: creat batchbatchRollback, batchProperties, batchAgents, jopSchedulers logic

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,16 @@ export class BatchService {
 	getHello(): string {
 		return 'welcome to Nestar Batch Service!';
 	}
+
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+
+	public async batchProperties(): Promise<void> {
+		console.log('batchProperties');
+	}
+
+	public async batchAgents(): Promise<void> {
+		console.log('batchAgents');
+	}
 }
