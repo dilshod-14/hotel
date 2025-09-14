f363651ebf1d0274b33807e75328c809f30445f5
dilshod-14 <urolovdilshod24@gmail.com>
2025-09-15 01:25:18 +0900

    fix: review.module modify

--- apps/nestar-api/src/components/review/review.module.ts
+++ apps/nestar-api/src/components/review/review.module.ts
@@ -3,15 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewService } from './review.service';
 import { ReviewResolver } from './review.resolver';
 import ReviewSchema from '../../schemas/review.model';
-import PropertySchema from '../../schemas/property.model'; // 🟢 qo‘shildi
 import { MemberModule } from '../member/member.module';
 import { AuthModule } from '../auth/auth.module';
+import PropertySchema from '../../schemas/Property.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Review', schema: ReviewSchema },
-      { name: 'Property', schema: PropertySchema }, // 🟢 MUHIM QO‘SHILDI
+      { name: 'Property', schema: PropertySchema }, 
     ]),
     MemberModule,
     AuthModule,
